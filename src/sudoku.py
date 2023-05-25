bde3237271ab4ab84939fffffe1c47c115e34d70
SajjiKazemi <SajjadKazemi.e@gmail.com>
2023-05-25 11:44:03 -0400

    Add some methods to sudoku class

--- src/sudoku.py
+++ src/sudoku.py
@@ -23,3 +23,37 @@ class sudoku():
         
     def print_sudoku(self):
         print(self.table)
+
+    def det_square(self, row: int, column: int):
+        "This function determines the square of the given row and column"
+        if row < 3:
+            if column < 3:
+                return 0
+            elif column < 6:
+                return 1
+            else:
+                return 2
+        elif row < 6:
+            if column < 3:
+                return 3
+            elif column < 6:
+                return 4
+            else:
+                return 5
+        else:
+            if column < 3:
+                return 6
+            elif column < 6:
+                return 7
+            else:
+                return 8
+    
+    def separate_squares(self):
+        "This function separates the sudoku into 9 squares"
+        self.squares = []
+        for i in range(9):
+            self.squares.append(self.table[self.det_square(i, 0):self.det_square(i, 0)+3, self.det_square(i, 1):self.det_square(i, 1)+3])
+        return self.squares[0]
+    
+    def check_square(self, square: int):
+        "This function checks if the square is valid in terms of sudoku rules"
